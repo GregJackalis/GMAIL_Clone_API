977276714c697f822f264622afbc145fe8a33fcf
TheJackal03 <gregorytsak3@gmail.com>
2023-08-06 01:49:09 +0300

    response model error not fixed, used another way with the return keyword on the create_user function

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,7 +5,7 @@ from ..database import connect_to_database
 
 router = APIRouter()
 
-@router.post("/create_User", status_code=status.HTTP_201_CREATED, response_model= schemas.UserCreateResponseModel)
+@router.post("/create_User", status_code=status.HTTP_201_CREATED)
 def create_user(input: schemas.UserCreateInput, database: Session = Depends(connect_to_database)):
     new_user = tables.User(**input.dict())
 
@@ -14,4 +14,7 @@ def create_user(input: schemas.UserCreateInput, database: Session = Depends(conn
 
     database.refresh(new_user)
 
-    return {"new user's info" : new_user}
+
+    ##SINCE I haven't fixed the response model problem, I'm returning the user's info without the password like this
+    return {"user was created": f"""username: {new_user.username},
+            id: {new_user.id}"""}
