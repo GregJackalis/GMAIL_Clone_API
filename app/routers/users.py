b2cd2c59079f4df125016cee259d9532b4f540b6
TheJackal03 <gregorytsak3@gmail.com>
2023-08-06 19:03:05 +0300

    hashing users' passwords

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.orm import Session
-from .. import tables, schemas
+from .. import tables, schemas, utils
 from ..database import connect_to_database
 
 router = APIRouter()
 
 @router.post("/create_User", status_code=status.HTTP_201_CREATED)
 def create_user(input: schemas.UserCreateInput, database: Session = Depends(connect_to_database)):
+
+    #hash the given password first
+    hashed_password = utils.hash_password(input.password)
+
+    #we replace the password given with the hashed password
+    input.password = hashed_password
+
+    #we make a variable that makes a dictionary out of the input and based on the User class' columns, in tables.py
     new_user = tables.User(**input.dict())
 
+    #we add the variable to the database
     database.add(new_user)
     database.commit()
 
---
+++ app/utils.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+#function that hashes a password
+def hash_password(password):
+    return pwd_context.hash(password)
+
+#function that verifies the given password is the same as the password that is hashed
+#in order to do that we take the input from the user hash it as well and check if the hashed password are the same
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
